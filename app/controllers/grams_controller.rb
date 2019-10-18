8cd3223e78648950bd58afd828a94d07db6a0e4c
Joe Slack <jslack2537@gmail.com>
2019-10-18 16:36:50 +0000

    added test for user auth on crud also refactored some code

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,18 +1,40 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
-def destroy
-  @gram = Gram.find_by_id(params[:id])
-  return render_not_found if @gram.blank?
-  @gram.destroy
-  redirect_to root_path
-end
+  def index
+  end
+
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+    @gram = current_user.grams.create(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+  end
 
   def update
-      @gram = Gram.find_by_id(params[:id])
+    @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
 
-      @gram.update_attributes(gram_params)
+    @gram.update_attributes(gram_params)
     if @gram.valid?
       redirect_to root_path
     else
@@ -20,36 +42,14 @@ end
     end
   end
 
-
-  def new
-    @gram = Gram.new
-  end
-
-  def index
-  end
-
-  def create
-  @gram = current_user.grams.create(gram_params)
-  if @gram.valid?
-    redirect_to root_path
-  else
-    render :new, status: :unprocessable_entity
-  end
-end
-
-  def show
+  def destroy
     @gram = Gram.find_by_id(params[:id])
-      if @gram.blank?
     return render_not_found if @gram.blank?
-    end
+    return render_not_found(:forbidden) if @gram.user != current_user
+    @gram.destroy
+    redirect_to root_path
   end
 
-def edit
-  @gram = Gram.find_by_id(params[:id])
-      if @gram.blank?
-    return render_not_found if @gram.blank?
-    end
-end 
 
   private
 
@@ -57,8 +57,9 @@ end
     params.require(:gram).permit(:message)
   end
 
-  def render_not_found
-    render plain: 'Not Found :(', status: :not_found
+
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
   end
 
 end
