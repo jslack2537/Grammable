da1c2f8851a666cc7630acaca70f2fd2e417dff9
Joe Slack <jslack2537@gmail.com>
2019-10-18 15:45:15 +0000

    added show.html test for @gram

--- spec/helpers/grams_helper_spec.rb
+++ spec/helpers/grams_helper_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
 
+  describe "grams#edit action" do
+    it "should successfully show the edit form if the gram is found" do
+
+    end
+
+    it "should return a 404 error message if the gram is not found" do
+
+    end
+  end
+
 
   describe "grams#show action" do
     it "should successfully show the page if the gram is found" do
@@ -9,7 +19,7 @@ RSpec.describe GramsController, type: :controller do
         get :show, params: { id: gram.id }
         expect(response).to have_http_status(:success)
     end
-    
+
     it "should return a 404 error if the gram is not found" do
       get :show, params: { id: 'TACOCAT' }
       expect(response).to have_http_status(:not_found)
